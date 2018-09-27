df3e5d955c4e817c8dc131b986acf96ecf3cbbaa
yankovskyandrey <YankovskyAndrey>
2018-09-27 11:51:04 +0300

    IO-58: вернул фикс в пикселях для настроек грида

--- components/Common.js
+++ components/Common.js
@@ -33,21 +33,21 @@ const Common = () =>
     .grid-container {
       display: grid;
       align-content: start;
-      grid-template-columns: repeat(12, 7.5rem);
-      grid-gap: 0 2rem;
+      grid-template-columns: repeat(12, 120px);
+      grid-gap: 0 32px;
     }
 
     @media (min-width: 1360px) and (max-width: 1919px) {
       .grid-container {
-        grid-template-columns: repeat(12, 6rem);
-        grid-gap: 0 1rem;
+        grid-template-columns: repeat(12, 96px);
+        grid-gap: 0 16px;
       }
     }
 
     @media (max-width: 1359px) {
       .grid-container {
-        grid-template-columns: repeat(12, 5.5rem);
-        grid-gap: 0 1rem;
+        grid-template-columns: repeat(12, 88px);
+        grid-gap: 0 16px;
       }
     }
 

1980992c09c43c7214fcbe422efef5bea2b843bc
Реш Артём <reshartyom@MacBook-Pro-Res.local>
2018-10-03 15:52:11 +0300

    IO-68: убрал local из font-face шрифтов

--- components/Common.js
+++ components/Common.js
@@ -43,7 +43,7 @@ const Common = () =>
 
     /* TODO:
       1. получить ссылку на шрифты с google fonts
-      2. вставить в браузер и скопировать нужные @font-face
+      2. вставить в браузер и скопировать нужные @font-face, удалив из url local шрифты
       3. зайати на browserstack, выбрать браузер, который поддерживает woff и повторить шаг 2
       4. PROFIT!
     */
@@ -115,7 +115,7 @@ const Common = () =>
       font-family: 'Roboto';
       font-style: italic;
       font-weight: 400;
-      src: local('Roboto Italic'), local('Roboto-Italic'), url(https://fonts.gstatic.com/s/roboto/v18/KFOkCnqEu92Fr1Mu51xIIzQXKMny.woff) format('woff');
+      src: url(https://fonts.gstatic.com/s/roboto/v18/KFOkCnqEu92Fr1Mu51xIIzQXKMny.woff) format('woff');
     }
 
     /* cyrillic */
@@ -267,7 +267,7 @@ const Common = () =>
       font-family: 'Roboto';
       font-style: italic;
       font-weight: 400;
-      src: local('Roboto Italic'), local('Roboto-Italic'), url(https://fonts.gstatic.com/s/roboto/v18/KFOkCnqEu92Fr1Mu51xMIzIXKMnyrYk.woff2) format('woff2');
+      src: url(https://fonts.gstatic.com/s/roboto/v18/KFOkCnqEu92Fr1Mu51xMIzIXKMnyrYk.woff2) format('woff2');
       unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
     }
 
@@ -276,7 +276,7 @@ const Common = () =>
       font-family: 'Roboto';
       font-style: italic;
       font-weight: 400;
-      src: local('Roboto Italic'), local('Roboto-Italic'), url(https://fonts.gstatic.com/s/roboto/v18/KFOkCnqEu92Fr1Mu51xIIzIXKMny.woff2) format('woff2');
+      src: url(https://fonts.gstatic.com/s/roboto/v18/KFOkCnqEu92Fr1Mu51xIIzIXKMny.woff2) format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
   `}</style>

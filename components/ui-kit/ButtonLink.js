081d16c4906adfdcc979f0e797ac5224d79f2c38
Реш Артём <reshartyom@MacBook-Pro-Res.local>
2018-10-19 11:47:38 +0200

    IO-124: заменил noopener noreferrer на noopener

--- components/ui-kit/ButtonLink.js
+++ components/ui-kit/ButtonLink.js
@@ -38,7 +38,7 @@ export default class Button extends PureComponent {
           target={external ? '_blank' : null}
           disabled={disabled}
           href={href}
-          rel='noreferrer noopener'
+          rel='noopener'
         >
           {children}
         </a>

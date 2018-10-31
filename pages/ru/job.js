378f07a05d25c48fc25a846a659643fbc7e5b3eb
yankovskyandrey <YankovskyAndrey>
2018-10-31 17:30:55 +0300

    IO-149: поправил eslint

--- pages/ru/job.js
+++ pages/ru/job.js
@@ -70,23 +70,23 @@ const processVacancy = vacancy => {
 
 // TODO
 // const onSubmit = async values => {
-  // const res = await fetch('/api/submit-form', {
-  //   method: 'POST',
-  //   headers: {
-  //     Accept: 'application/json',
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(values),
-  // })
-  //
-  // if (res.status === 201) {
-  //   if (window.dataLayer) {
-  //     window.dataLayer.push({ event: 'form_success' })
-  //   }
-  // } else if (res.status === 400) {
-  //   const error = await res.json()
-  //   return { [FORM_ERROR]: error.error }
-  // }
+// const res = await fetch('/api/submit-form', {
+//   method: 'POST',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(values),
+// })
+//
+// if (res.status === 201) {
+//   if (window.dataLayer) {
+//     window.dataLayer.push({ event: 'form_success' })
+//   }
+// } else if (res.status === 400) {
+//   const error = await res.json()
+//   return { [FORM_ERROR]: error.error }
+// }
 // }
 
 class Job extends PureComponent {

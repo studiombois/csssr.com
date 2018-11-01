8a59de3a2b99d9d69ef45e94a116d02f91990a40
Oleg Vikoultsev <vikoultsev@gmail.com>
2018-11-01 19:52:31 +0800

    IO-141: переделал класс в stateless component

--- pages/en/jobs.js
+++ pages/en/jobs.js
@@ -1,32 +1,28 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { Fragment } from 'react'
 import Head from '../../components/Head'
 import withI18next from '../../utils/withI18next'
 import Layout from '../../components/Layout'
 import NoPage from '../../components/NoPage'
 
-class Jobs extends PureComponent {
-
-  render() {
-    const { t } = this.props
-    return (
-      <Fragment>
-        <Layout
-          headerProps={{
-            isHalfed: true,
-            logoHref: '/ru/jobs#',
-            logoAlt: 'CSSSR jobs logo',
-            logoSup: '.jobs',
-            isLogoLink: true,
-            isBurgerVisible: true,
-          }}
-          footerProps={{ noFooter: true }}
-        >
-          <Head title={t('jobs:meta.title')} description={t('jobs:meta.description')} />
-          <NoPage />
-        </Layout>
-      </Fragment>
-    )
-  }
+function Jobs({ t }) {
+  return (
+    <Fragment>
+      <Layout
+        headerProps={{
+          isHalfed: true,
+          logoHref: '/ru/jobs#',
+          logoAlt: 'CSSSR jobs logo',
+          logoSup: '.jobs',
+          isLogoLink: true,
+          isBurgerVisible: true,
+        }}
+        footerProps={{ noFooter: true }}
+      >
+        <Head title={t('jobs:meta.title')} description={t('jobs:meta.description')} />
+        <NoPage />
+      </Layout>
+    </Fragment>
+  )
 }
 
 export default withI18next(['jobs'])(Jobs)

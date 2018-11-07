f8b5a51ed1f0e9e99a4d24d21a030b233836e182
yankovskyandrey <YankovskyAndrey>
2018-11-07 14:13:01 +0300

    Добавил HR_ORIGIN переменную окружения

--- utils/hrOrigin.js
+++ utils/hrOrigin.js
@@ -1,1 +1,9 @@
-export default process.env.NODE_ENV === 'development' ? 'http://localhost:3030' : 'https://hr.csssr.ru'
+export default (() => {
+  if (process.env.HR_ORIGIN) {
+    return process.env.HR_ORIGIN
+  } else if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:3030'
+  }
+  return 'https://hr.csssr.ru'
+
+})()

9d15a5383939f06ed28f53ff78e64ac6fdc45741
ArtyomResh <artyom.resh@csssr.com>
2018-11-22 15:42:08 +0300

    IO-218: Сверстал мобильную версию блока партнёров

---
+++ components/dev/PartnersMobile.js
@@ -0,0 +1,59 @@
+import React, { Fragment } from 'react'
+import LinksList from './LinksList'
+import { translate } from 'react-i18next'
+import {
+  iTcompaniesLinksEn,
+  iTcompaniesLinksRu,
+  realEstateLinksEn,
+  telecomLinksEn,
+  telecomLinksRu,
+  financeAndBankingLinksEn,
+  financeAndBankingLinksRu,
+  othersLinksEn,
+  othersLinksRu,
+  healthLinksRu,
+} from '../../data/jobs/partnersLinks'
+
+const Partners = ({ t, lng }) =>
+  <Fragment>
+    <section className='grid-container'>
+      <h2 id='customers' className='font_h2-slab' dangerouslySetInnerHTML={{ __html: t('dev:partners.title') }} />
+
+      <LinksList
+        title={t('dev:partners.iTTitle')}
+        links={lng === 'ru' ? iTcompaniesLinksRu : iTcompaniesLinksEn}
+      />
+      <LinksList
+        title={t('dev:partners.financeTitle')}
+        links={lng === 'ru' ? financeAndBankingLinksRu : financeAndBankingLinksEn}
+      />
+      <LinksList
+        title={t('dev:partners.telecomTitle')}
+        links={lng === 'ru' ? telecomLinksRu : telecomLinksEn}
+      />
+      <LinksList
+        title={t('dev:partners.estateTitle')}
+        links={lng === 'ru' ? healthLinksRu : realEstateLinksEn}
+      />
+      <LinksList
+        title={t('dev:partners.othersTitle')}
+        links={lng === 'ru' ? othersLinksRu : othersLinksEn}
+      />
+    </section><style jsx>{`
+      section {
+        margin-left: auto;
+        margin-right: auto;
+        padding-top: 3rem;
+        padding-bottom: 0.9375rem;
+        width: 20.5rem;
+      }
+
+      h2 {
+        margin-bottom: 2.0625rem;
+        grid-column: 1 / span 6;
+        text-align: center;
+      }
+    `}</style>
+  </Fragment>
+
+export default translate()(Partners)
--- pages/ru/index.js
+++ pages/ru/index.js
@@ -1,32 +1,58 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Head from '../../components/Head'
 import Form from '../../components/dev/Form'
 import Hire from '../../components/dev/Hire'
 import Feature1 from '../../components/dev/Feature1'
 import Feature2 from '../../components/dev/Feature2'
 import Partners from '../../components/dev/Partners'
+import PartnersMobile from '../../components/dev/PartnersMobile'
 import Layout from '../../components/Layout'
 import withI18next from '../../utils/withI18next'
 
-export default withI18next(['dev'])(({ t }) =>
-  <Layout headerProps={{ logoHref: '/ru', isBurgerVisible: true }} footerProps={{ logoHref: '/ru' }}>
-    <Head title={t('dev:meta.title')} description={t('dev:meta.description')} />
-    <Hire />
-    <Feature1
-      image={{ namespace: 'dev', key: 'geometry', alt: t('dev:imgAlt.geometry') }}
-    />
-    <Feature2
-      title={t('dev:time.title')}
-      text={t('dev:time.text')}
-      image={{ namespace: 'dev', key: 'time', alt: t('dev:imgAlt.time') }}
-    />
-    <Partners />
-    <Feature2
-      style={{ marginTop: '-0.5rem' }}
-      title={t('dev:perfect.title')}
-      text={t('dev:perfect.text')}
-      image={{ namespace: 'dev', key: 'perfect', alt: t('dev:imgAlt.perfect') }}
-    />
-    <Form />
-  </Layout>
-)
+class Dev extends PureComponent {
+  state = {
+    isMobile: false,
+  }
+
+  componentDidMount() {
+    const mobileMediaQuery = window.matchMedia('(max-width: 767px)')
+
+    mobileMediaQuery.addListener(this.handleMediaMatch)
+    this.handleMediaMatch(mobileMediaQuery)
+  }
+
+  handleMediaMatch = ({ matches }) =>
+    this.setState({
+      isMobile: matches,
+    })
+
+  render() {
+    const { t } = this.props
+
+    return (
+      <Layout headerProps={{ logoHref: '/ru', isBurgerVisible: true }} footerProps={{ logoHref: '/ru' }}>
+        <Head title={t('dev:meta.title')} description={t('dev:meta.description')} />
+        <Hire />
+        <Feature1
+          image={{ namespace: 'dev', key: 'geometry', alt: t('dev:imgAlt.geometry') }}
+        />
+        <Feature2
+          title={t('dev:time.title')}
+          text={t('dev:time.text')}
+          image={{ namespace: 'dev', key: 'time', alt: t('dev:imgAlt.time') }}
+        />
+        {this.state.isMobile ? <PartnersMobile /> : <Partners />}
+
+        <Feature2
+          style={{ marginTop: '-0.5rem' }}
+          title={t('dev:perfect.title')}
+          text={t('dev:perfect.text')}
+          image={{ namespace: 'dev', key: 'perfect', alt: t('dev:imgAlt.perfect') }}
+        />
+        <Form />
+      </Layout>
+    )
+  }
+}
+
+export default withI18next(['dev'])(Dev)

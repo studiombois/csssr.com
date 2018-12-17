f122269cfaaf0821a21f25a2545bab82b9d59c95
ArtyomResh <artyom.resh@csssr.com>
2018-12-17 19:01:38 +0300

    IO-300: создал блок Portfolio

---
+++ data/sborka/porfolioProjects.js
@@ -0,0 +1,7 @@
+export default [
+  { name: 'relef', href: 'https://relef.ru/' },
+  { name: 'investments', href: 'https://investments101.ru' },
+  { name: 'houzz', href: 'https://www.houzz.ru' },
+  { name: 'nutrilogic', href: 'http://nutrilogic.ru/' },
+  { name: 'siberianHealth', href: 'https://ru.siberianhealth.com' },
+]

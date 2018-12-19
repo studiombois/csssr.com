b333308358b75b8fbe70e3c2ad46bebf9a63f5a3
ArtyomResh <artyom.resh@csssr.com>
2018-12-19 12:15:32 +0300

    IO-327: Правки по тексту
    — Поправил текста согласно замечаниям Томы;
    — Удалил картинки и текста "Сибирского здоровья" из портфолио

--- data/sborka/porfolioProjects.js
+++ data/sborka/porfolioProjects.js
@@ -1,7 +1,6 @@
 export default [
   { name: 'relef', href: 'https://relef.ru/' },
   { name: 'investments', href: 'https://investments101.ru' },
-  { name: 'houzz', href: 'https://www.houzz.ru' },
+  { name: 'houzz', href: 'https://remont.tinkoff.ru/' },
   { name: 'nutrilogic', href: 'http://nutrilogic.ru/' },
-  { name: 'siberianHealth', href: 'https://ru.siberianhealth.com' },
 ]

b85d9074c57c3d87470c8bc42874b71c050bd017
ArtyomResh <artyom.resh@csssr.com>
2018-12-24 16:33:49 +0300

    IO-288: убрал возможность включения сетки и гридов по хоткеям; обновил доку

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## Гриды и лейауты
+В компоненте Settings есть два div элемента, у которых закоменченны классы,
+их можно раскоментить и увидеть сетку и грид-колонки, этим удобно пользоваться во время разработки, что бы сверять расстановку блоков по гридам, а текста по бейзлайну.
+
+## Next.js README
+
 This project was bootstrapped with [Create Next App](https://github.com/segmentio/create-next-app).
 
 Find the most recent version of this guide at [here](https://github.com/segmentio/create-next-app/blob/master/lib/templates/default/README.md). And check out [Next.js repo](https://github.com/zeit/next.js) for the most up-to-date info.
--- components/Settings.js
+++ components/Settings.js
@@ -1,133 +1,118 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { Fragment } from 'react'
 
-export default class Settings extends PureComponent {
-  componentDidMount() {
-    window.addEventListener('keydown', function (event) {
-      if ((event.ctrlKey || event.metaKey) && event.which === 71) {
-        event.preventDefault()
-        document.getElementById('background-for-grid').classList.toggle('grid')
-      } else if ((event.ctrlKey || event.metaKey) && event.which === 76) {
-        event.preventDefault()
-        document.getElementById('background-for-layout').classList.toggle('layout-columns-on')
-      }
-    })
-  }
+export default () =>
+  <Fragment>
+    <div id='background-for-layout'
+      // className='layout-columns-on'
+    />
+    <div id='background-for-grid'
+      // className='grid-on'
+    />
 
-  render() {
-    return <Fragment>
-      <div id='background-for-layout'
-        // className='layout-columns-on'
-      />
-      <div id='background-for-grid'
-        // className='grid'
-      />
+    <style jsx global>{`
+    #background-for-layout,
+    #background-for-grid {
+      display: none;
+      position: absolute;
+      z-index: 99999;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
 
-      <style jsx global>{`
+    #background-for-layout,
+    #background-for-grid {
+      width: 1792px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+
+    #background-for-layout:after {
+      top: 0;
+      right: 0;
+    }
+
+    #background-for-grid.grid-on {
+      display: block;
+      background-size: 8px 8px;
+      background-image: linear-gradient(to right, lightgrey 1px, transparent 1px), linear-gradient(to bottom, lightgrey 1px, transparent 1px);
+      opacity: 0.5;
+    }
+
+    #background-for-layout.layout-columns-on {
+      display: block;
+      background-size: 152px;
+      background-image: linear-gradient(90deg, #F6D3C3 120px, transparent 120px, transparent 32px);
+      opacity: 0.5;
+    }
+
+    @media (min-width: 1360px) and (max-width: 1919px) {
       #background-for-layout,
       #background-for-grid {
-        display: none;
-        position: absolute;
-        z-index: 99999;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-      }
-
-      #background-for-layout,
-      #background-for-grid {
-        width: 1792px;
-        left: 50%;
-        transform: translateX(-50%);
-      }
-
-      #background-for-layout:after {
-        top: 0;
-        right: 0;
-      }
-
-      #background-for-grid.grid {
-        display: block;
-        background-size: 8px 8px;
-        background-image: linear-gradient(to right, lightgrey 1px, transparent 1px), linear-gradient(to bottom, lightgrey 1px, transparent 1px);
-        opacity: 0.5;
+        width: 1328px;
       }
 
       #background-for-layout.layout-columns-on {
-        display: block;
-        background-size: 152px;
-        background-image: linear-gradient(90deg, #F6D3C3 120px, transparent 120px, transparent 32px);
+        background-size: 112px;
+        background-image: linear-gradient(90deg, #F6D3C3 96px, transparent 96px, transparent 16px);
         opacity: 0.5;
       }
+    }
 
-      @media (min-width: 1360px) and (max-width: 1919px) {
-        #background-for-layout,
-        #background-for-grid {
-          width: 1328px;
-        }
-
-        #background-for-layout.layout-columns-on {
-          background-size: 112px;
-          background-image: linear-gradient(90deg, #F6D3C3 96px, transparent 96px, transparent 16px);
-          opacity: 0.5;
-        }
+    @media (min-width: 1280px) and (max-width: 1359px) {
+      #background-for-layout,
+      #background-for-grid {
+        width: 1232px;
       }
 
-      @media (min-width: 1280px) and (max-width: 1359px) {
-        #background-for-layout,
-        #background-for-grid {
-          width: 1232px;
-        }
+      #background-for-layout.layout-columns-on {
+        background-size: 104px;
+        background-image: linear-gradient(90deg, #F6D3C3 88px, transparent 88px, transparent 16px);
+      }
+    }
 
-        #background-for-layout.layout-columns-on {
-          background-size: 104px;
-          background-image: linear-gradient(90deg, #F6D3C3 88px, transparent 88px, transparent 16px);
-        }
+    @media (min-width: 1024px) and (max-width: 1279px) {
+      #background-for-grid,
+      #background-for-layout {
+        width: 944px;
       }
 
-      @media (min-width: 1024px) and (max-width: 1279px) {
-        #background-for-grid,
-        #background-for-layout {
-          width: 944px;
-        }
+      #background-for-layout.layout-columns-on {
+        background-size: 80px;
+        background-image: linear-gradient(90deg, #F6D3C3 64px, transparent 64px, transparent 16px);
+      }
+    }
 
-        #background-for-layout.layout-columns-on {
-          background-size: 80px;
-          background-image: linear-gradient(90deg, #F6D3C3 64px, transparent 64px, transparent 16px);
-        }
+    @media (min-width: 768px) and (max-width: 1023px) {
+      #background-for-grid,
+      #background-for-layout {
+        width: 59rem;
       }
 
-      @media (min-width: 768px) and (max-width: 1023px) {
-        #background-for-grid,
-        #background-for-layout {
-          width: 59rem;
-        }
+      #background-for-layout.layout-columns-on {
+        background-size: 5rem;
+        background-image: linear-gradient(90deg, #F6D3C3 4rem, transparent 4rem, transparent 1rem);
+      }
+    }
 
-        #background-for-layout.layout-columns-on {
-          background-size: 5rem;
-          background-image: linear-gradient(90deg, #F6D3C3 4rem, transparent 4rem, transparent 1rem);
-        }
+    @media (max-width: 1023px) {
+      #background-for-grid.grid-on {
+        background-size: 0.5rem 0.5rem;
+        background-image: linear-gradient(to right, lightgrey 0.0625rem, transparent 0.0625rem), linear-gradient(to bottom, lightgrey 0.0625rem, transparent 0.0625rem);
+      }
+    }
+
+    @media (max-width: 767px) {
+      #background-for-grid,
+      #background-for-layout {
+        width: 20.5rem;
       }
 
-      @media (max-width: 1023px) {
-        #background-for-grid.grid {
-          background-size: 0.5rem 0.5rem;
-          background-image: linear-gradient(to right, lightgrey 0.0625rem, transparent 0.0625rem), linear-gradient(to bottom, lightgrey 0.0625rem, transparent 0.0625rem);
-        }
+      #background-for-layout.layout-columns-on {
+        background-size: 3.5rem;
+        background-image: linear-gradient(90deg, #F6D3C3 3rem, transparent 3rem, transparent 0.5rem);
       }
-
-      @media (max-width: 767px) {
-        #background-for-grid,
-        #background-for-layout {
-          width: 20.5rem;
-        }
-
-        #background-for-layout.layout-columns-on {
-          background-size: 3.5rem;
-          background-image: linear-gradient(90deg, #F6D3C3 3rem, transparent 3rem, transparent 0.5rem);
-        }
-      }
-    `}</style>
-    </Fragment>
-  }
-}
+    }
+  `}</style>
+  </Fragment>

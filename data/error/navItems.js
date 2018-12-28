546300074c0da53218d4ea73de85f70c85becbea
ArtyomResh <artyom.resh@csssr.com>
2018-12-28 18:35:46 +0300

    IO-195: Запилил страницу 404; поправил расположение partnersLinks

---
+++ data/error/navItems.js
@@ -0,0 +1,31 @@
+export const navItemsRu = [{
+  sectionTitle: 'error:nav.dev',
+  href: '/ru',
+  // TODO: вставить разделы подсекций, когда появятся соответствующие разделы
+  // subsections: ['', '', ''],
+}, {
+  sectionTitle: 'error:nav.html',
+  href: '/ru/html',
+}, {
+  sectionTitle: 'error:nav.school',
+  href: '/ru/school',
+}, {
+  sectionTitle: 'error:nav.jobs',
+  href: '/ru/jobs',
+}]
+
+export const navItemsEn = [{
+  sectionTitle: 'error:nav.dev',
+  href: '/en',
+  // TODO: вставить разделы подсекций, когда появятся соответствующие разделы
+  // subsections: ['', '', ''],
+}, {
+  sectionTitle: 'error:nav.html',
+  href: '/en/html',
+}, {
+  sectionTitle: 'error:nav.school',
+  href: '/ru/school',
+}, {
+  sectionTitle: 'error:nav.jobs',
+  href: '/ru/jobs',
+}]

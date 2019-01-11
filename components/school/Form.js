baf2b4c01511b4d2351dc5cdf1badb2758fad2ca
ArtyomResh <artyom.resh@csssr.com>
2019-01-11 16:47:48 +0300

    IO-402: настроил отправку тегов в AMO по типам заявок на школе

--- components/school/Form.js
+++ components/school/Form.js
@@ -23,35 +23,43 @@ const ContactFormForSchool = props =>
     `}</style>
   </div>
 
-const onSubmit = async values => {
-  const res = await fetch('/api/school-submit-form', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(values),
-  })
 
-  if (res.status === 201) {
-    if (window.dataLayer) {
-      window.dataLayer.push({ event: 'school_form_success' })
+
+const Form = ({ choosenCourse }) => {
+  const onSubmit = async values => {
+    values.course = choosenCourse
+
+    const res = await fetch('/api/school-submit-form', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    })
+
+    if (res.status === 201) {
+      if (window.dataLayer) {
+        window.dataLayer.push({ event: 'school_form_success' })
+      }
+    } else if (res.status === 400) {
+      const error = await res.json()
+
+      if (window.dataLayer) {
+        window.dataLayer.push({ event: 'school_form_fail' })
+      }
+
+      return { [FORM_ERROR]: error.error }
     }
-  } else if (res.status === 400) {
-    const error = await res.json()
-
-    if (window.dataLayer) {
-      window.dataLayer.push({ event: 'school_form_fail' })
-    }
-
-    return { [FORM_ERROR]: error.error }
   }
-}
 
-const Form = () => <ReactFinalForm
-  onSubmit={onSubmit}
-  validate={contactFormValidationRules}
-  component={ContactFormForSchool}
-/>
+  return (
+    <ReactFinalForm
+      onSubmit={onSubmit}
+      validate={contactFormValidationRules}
+      component={ContactFormForSchool}
+    />
+  )
+}
 
 export default translate()(Form)

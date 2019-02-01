5df52ddac5f52aceaeeaf1c7a934ef0d1ebc5258
yankovskyandrey <YankovskyAndrey@gmail.com>
2019-02-01 16:44:59 +0300

    IO-457: поправил работу ссылок из портфолио на десктопе; убрал лишний слэш у ссылок из портфолио

--- data/sborka/porfolioProjects.js
+++ data/sborka/porfolioProjects.js
@@ -1,6 +1,6 @@
 export default [
-  { name: 'relef', href: 'https://relef.ru/' },
+  { name: 'relef', href: 'https://relef.ru' },
   { name: 'investments', href: 'https://investments101.ru' },
-  { name: 'houzz', href: 'https://remont.tinkoff.ru/' },
-  { name: 'nutrilogic', href: 'http://nutrilogic.ru/' },
+  { name: 'houzz', href: 'https://remont.tinkoff.ru' },
+  { name: 'nutrilogic', href: 'http://nutrilogic.ru' },
 ]

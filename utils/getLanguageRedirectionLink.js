95fe95306d5a0b02b1a3cd8ccbb8096d9dd4812c
ArtyomResh <artyom.resh@csssr.com>
2019-02-07 11:44:58 +0300

    IO-353: убрал слеш из генерируемого пути в getLanguageRedirectionLink

--- utils/getLanguageRedirectionLink.js
+++ utils/getLanguageRedirectionLink.js
@@ -1,6 +1,6 @@
 const LANGUAGE_PATHNAME_WIDTH = 3
 const changeLanguagePathnameTo = (pathname, languagePathname) =>
-  '/' + languagePathname + pathname.slice(LANGUAGE_PATHNAME_WIDTH)
+  languagePathname + pathname.slice(LANGUAGE_PATHNAME_WIDTH)
 
 export default (pathname, language) => {
   switch (language) {

a28219ecea102984b5c6d107e04718ac31053616
Artyom Resh <artyomresh@gmail.com>
2019-05-29 18:26:10 +0300

    COM-254: добавил варнинг о sitemap на прекоммитe

---
+++ server/check-sitemap.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const sitemapUrls = require('./generate-sitemap').sitemapUrls
+
+// module.exports = () => {
+const pagesUrls = []
+
+const walkSync = dir => {
+  const files = fs.readdirSync(dir)
+
+  files.forEach(file => {
+    const filePath = `${dir}${file}`
+    const fileStat = fs.statSync(filePath)
+    const isFileDirectory = fileStat.isDirectory()
+
+    if (isFileDirectory) {
+      walkSync(`${filePath}/`)
+    } else {
+      const url = 'https://csssr.com/' + filePath
+        .substr(0, filePath.lastIndexOf('.'))
+        .replace('pages/', '')
+        .replace('/index', '')
+
+      const excludedPagesUrls = [
+        'https://csssr.com/_app',
+        'https://csssr.com/_document',
+        'https://csssr.com/_error',
+        'https://csssr.com/fonts',
+        'https://csssr.com/en/job',
+        'https://csssr.com/ru/job',
+        'https://csssr.com/en/school',
+      ]
+
+      if (excludedPagesUrls.includes(url)) {
+        return
+      }
+
+      pagesUrls.push(url)
+    }
+  })
+}
+
+walkSync('pages/')
+
+pagesUrls.some(pageUrl => {
+  if (!sitemapUrls.includes(pageUrl)) {
+    throw `
+      \x1b[33m
+      Страница ${pageUrl} не указана в файле генерации sitemap: ./server/generate-sitemap.js
+
+      Добавтье информацию о ней в объект sitemapUrlsSettings 
+      или внесите её в список исключений excludedPagesUrls в  ./server/check-sitemap.js
+      \x1b[0m
+    `
+  }
+})
+
+
+// }

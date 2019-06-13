348c5b574c7bc8b44067607407132eb7dd9caafd
Artyom Resh <artyomresh@gmail.com>
2019-06-13 14:12:12 +0300

    COM-600: добавил описание типов для некоторых переменных

--- utils/getLanguageRedirectionLink.js
+++ utils/getLanguageRedirectionLink.js
@@ -12,7 +12,15 @@ const getRedirectionPath = (pathname, language) => {
 }
 
 export default async (pathname, language) => {
+  /**
+  * @type {String}
+  * @example: `/en/jobs/senior-js-developer`
+  */
   const preciseRedirectionPath = getRedirectionPath(pathname, language)
+  /**
+  * @type {String}
+  * @example: `/en`
+  */
   const baseRedirectionPath = preciseRedirectionPath.split('/').slice(0, 2).join('/')
 
   try {

c95f5853e5510c7c66cdbac688c122cb1a6baf89
Artyom Resh <artyomresh@gmail.com>
2019-10-17 12:49:29 +0300

    fix(link): поправляет адрес ссылки на jobs-faq

--- components/jobs/Vacancies.js
+++ components/jobs/Vacancies.js
@@ -286,7 +286,7 @@ class Vacancies extends PureComponent {
                 {t('jobs:faq.title')}
 
                 <a
-                  href='jobs-faq'
+                  href='../jobs-faq'
                   className='font_link-list_16'
                 >
                   {t('jobs:faq.link')}

7a34deda8f0d9fd7ace85c5c939d96c2ba70780f
exbtrg <exbtrg@gmail.com>
2019-10-28 19:29:17 +0300

    fix/COM-815: дописал стили для ie в модалке

--- components/ContactModal/Form/styles.js
+++ components/ContactModal/Form/styles.js
@@ -12,12 +12,10 @@ const formCSS = css.resolve`
   h2 {
     grid-column: 3 / span 6;
     margin-bottom: 4.5rem;
-    -ms-grid-column: 5;
   }
 
   .field {
     grid-column: 2 / span 4;
-    -ms-grid-column: 5;
     margin-bottom: 2.0625rem;
   }
 
@@ -49,7 +47,6 @@ const formCSS = css.resolve`
   .button {
     margin-top: 0;
     grid-column: 5 / span 2;
-    -ms-grid-column: 9;
   }
 
   @media (min-width: 1360px) and (max-width: 1919px) {
@@ -146,6 +143,161 @@ const formCSS = css.resolve`
       .button {
         margin-top: 0;
         grid-column: 2 / span 4;
+      }
+    }
+  }
+
+
+  //ms-styles
+  form {
+    -ms-grid-columns: 120px (32px 120px)[9];
+    -ms-grid-rows: (auto)[7];
+  }
+
+  h2 {
+    -ms-grid-column: 5;
+    -ms-grid-column-span: 11;
+    -ms-grid-row: 1;
+  }
+
+  .field {
+    -ms-grid-column: 3;
+    -ms-grid-column-span: 7;
+  }
+
+  .field:nth-of-type(1) {
+    -ms-grid-row: 2; 
+  }
+
+  .field:nth-of-type(2) {
+    -ms-grid-row: 3; 
+  }
+
+  .field:nth-of-type(3) {
+    -ms-grid-row: 4; 
+  }
+
+  .field:nth-of-type(4) {
+    -ms-grid-row: 2;
+    -ms-grid-row-span: 3;
+  }
+
+  .field:nth-of-type(5) {
+    -ms-grid-row: 5; 
+  }
+
+  .field:nth-of-type(6) {
+    -ms-grid-row: 6; 
+  }  
+
+  .field_type_textarea {
+    -ms-grid-column: 11;
+    -ms-grid-column-span: 7;
+  }
+
+  .field_type_checkbox {
+    -ms-grid-column: 3;
+    -ms-grid-column-span: 15;
+  }
+
+  .button {
+    -ms-grid-column: 9;
+    -ms-grid-column-span: 3;
+    -ms-grid-row: 7;
+  }
+
+  @media (max-width: 1279px) {
+    form {
+      -ms-grid-rows: (auto)[8];
+    }
+    // .field {
+    //   -ms-grid-column: 2;
+    //   -ms-grid-column-span: 7;
+    // }
+    .field:nth-of-type(1) {
+      -ms-grid-row: 2; 
+    }
+  
+    .field:nth-of-type(2) {
+      -ms-grid-row: 3; 
+    }
+  
+    .field:nth-of-type(3) {
+      -ms-grid-row: 4; 
+    }
+  
+    .field:nth-of-type(4) {
+      -ms-grid-row: 5;
+      -ms-grid-row-span: 1;
+    }
+  
+    .field:nth-of-type(5) {
+      -ms-grid-row: 6; 
+    }
+  
+    .field:nth-of-type(6) {
+      -ms-grid-row: 7; 
+    }
+
+    .button {
+      -ms-grid-column: 9;
+      -ms-grid-column-span: 3;
+      -ms-grid-row: 8;
+    }
+  }
+
+  @media (min-width: 1360px) and (max-width: 1919px) {
+    form {
+      -ms-grid-columns: 96px (16px 96px)[9];
+    }
+  }
+
+  @media (min-width: 1280px) and (max-width: 1359px) {
+    form {
+      -ms-grid-columns: 88px (16px 88px)[9];
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1279px) {
+    form {
+      -ms-grid-columns: 4rem (1rem 4rem)[7];
+    }
+
+    h2 {
+      -ms-grid-column: 3;
+      -ms-grid-column-span: 11;
+    }
+
+    .field {
+      -ms-grid-column: 3;
+      -ms-grid-column-span: 11;
+    }
+
+    .button {
+      -ms-grid-column: 7;
+      -ms-grid-column-span: 3;
+    }
+  }
+
+  @media (max-width: 767px) {
+      form {
+        -ms-grid-columns: 3rem (0.5rem 3rem)[5];
+      }
+
+      h2 {
+        -ms-grid-column: 1;
+        -ms-grid-column-span: 11;
+      }
+
+      .field {
+        -ms-grid-column: 1;
+        -ms-grid-column-span: 11;
+      }
+
+      .button {
+        grid-column: 2 / span 4;
+        -ms-grid-column: 3;
+        -ms-grid-column-span: 7;
       }
     }
   }

e2a78aa3263debda6cacafc4fc2daae75022b7cd
exbtrg <exbtrg@gmail.com>
2019-12-02 11:49:27 +0300

    fix/COM-1005: вернул data-scroll-lock-fill-gap дабавил important для padding

--- components/HeaderContent.js
+++ components/HeaderContent.js
@@ -54,6 +54,7 @@ class HeaderContent extends PureComponent {
     return (
       <Fragment>
         <header
+          data-scroll-lock-fill-gap
           onFocus={onFocus}
           className={cn({
             header: true,
@@ -160,7 +161,7 @@ class HeaderContent extends PureComponent {
               right: 0;
               z-index: 9999;
               width: 100%;
-              padding-right: ${!pinHeader && showHeader && isSideBarOpened ? scrollbarWidth + 'px' : '0'};
+              padding-right: ${!pinHeader && showHeader && isSideBarOpened ? scrollbarWidth + 'px' : '0'} !important;
             }
 
             .header-background {

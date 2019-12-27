7ba95b5920fae9159e3bf3bc90bdc26a28b99613
Artyom Resh <artyomresh@gmail.com>
2019-12-27 17:08:30 +0300

    fix: COM-982 отключает Sentry на develop окружении

--- utils/sentry.js
+++ utils/sentry.js
@@ -1,10 +1,19 @@
 const Sentry = require('@sentry/node')
 const { APP_ENV, isDevelopment } = require('../utils/app-environment')
 
-Sentry.init({
-  dsn: 'https://7b0bc195d134489f86572d94c310969b@sentry.io/1330752',
-  release: process.env.SENTRY_RELEASE,
-  environment: APP_ENV,
-  debug: isDevelopment,
-  attachStacktrace: true,
-})
+/**
+ * При `!!isDevelopment` не будет отправляться никаких оповещений
+ * даже если мы будетм их отправлять самостоятельно посредством captureMessage.
+ * Если потребуется отправить с develop окружения что-то в Sentry, то можно будет
+ * на месте сделать Sentry.init() и Sentry.captureMessage()
+ */
+
+if ( !isDevelopment ) {
+  Sentry.init({
+    dsn: 'https://7b0bc195d134489f86572d94c310969b@sentry.io/1330752',
+    release: process.env.SENTRY_RELEASE,
+    environment: APP_ENV,
+    debug: isDevelopment,
+    attachStacktrace: true,
+  })
+}

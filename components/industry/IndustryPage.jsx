c67f357062899710ea8578d7f27a0d58a554e806
exbtrg <exbtrg@gmail.com>
2020-01-14 13:46:31 +0300

    feat/COM-1055: удаляет лишний контекст из industry/Greeting

--- components/industry/IndustryPage.jsx
+++ components/industry/IndustryPage.jsx
@@ -1,14 +1,12 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Layout from '../Layout'
 import Greeting from './Greeting'
 import withI18next from '../../utils/withI18next'
-import { Ie11BrowserContext } from '../../utils/msBrowserProvider'
 
 const IndustryPage = () => {
-  const isIe11 = useContext(Ie11BrowserContext)
   return (
     <Layout>
-      <Greeting isIe11={isIe11} />
+      <Greeting />
     </Layout>
   )
 }

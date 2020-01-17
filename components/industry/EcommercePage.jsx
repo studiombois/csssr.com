6c86d21c7561bbaf6af4a0ecb46ec61c64227695
exbtrg <exbtrg@gmail.com>
2020-01-17 14:41:06 +0300

    feat/COM-1058: вносит правки по комментам из PR - исправляет размер заголовка - выносит content в data - добавляет размер l для picture

--- components/industry/EcommercePage.jsx
+++ components/industry/EcommercePage.jsx
@@ -8,9 +8,10 @@ import Greeting from './Greeting'
 import OurClients from './OurClients'
 import Facts from './Facts'
 
-import { greetingImages } from '../../data/industry/ecommerce/images/greeting'
-import { ourClientsImages } from '../../data/industry/ecommerce/images/logos'
-import { factsImages } from '../../data/industry/ecommerce/images/facts'
+import { greetingImages } from '../../data/industry/ecommerce/greeting'
+import { ourClientsImages } from '../../data/industry/ecommerce/logos'
+
+import facts from '../../data/industry/ecommerce/facts'
 
 const IndustryPage = ({ t }) => {
   return (
@@ -35,37 +36,7 @@ const IndustryPage = ({ t }) => {
         }}
       />
 
-      <Facts
-        id="facts"
-        content={{
-          heading: t('ecommerce:facts.heading'),
-          subHeading: t('ecommerce:facts.subHeading'),
-          images: factsImages,
-          imgAlt: t('ecommerce:imgAlt.facts'),
-          factItems: [
-            {
-              number: t('ecommerce:facts.firstItem.number'),
-              text: t('ecommerce:facts.firstItem.text'),
-              className: 'first-item',
-            },
-            {
-              number: t('ecommerce:facts.secondItem.number'),
-              text: t('ecommerce:facts.secondItem.text'),
-              className: 'second-item',
-            },
-            {
-              number: t('ecommerce:facts.thirdItem.number'),
-              text: t('ecommerce:facts.thirdItem.text'),
-              className: 'third-item',
-            },
-            {
-              number: t('ecommerce:facts.fourthItem.number'),
-              text: t('ecommerce:facts.fourthItem.text'),
-              className: 'fourth-item',
-            },
-          ],
-        }}
-      />
+      <Facts id="facts" content={facts} />
     </Layout>
   )
 }
--- components/industry/Facts/Facts.jsx
+++ components/industry/Facts/Facts.jsx
@@ -1,53 +1,44 @@
 import React from 'react'
-import { string, object, array } from 'prop-types'
+import { string, object, array, func } from 'prop-types'
 import styled from '@emotion/styled'
 import styles from './Facts.styles'
-import cn from 'classnames'
+import translate from '../../../utils/translate-wrapper'
 import Grid from '../../ui-kit/core-design/Grid'
-import Text from '../../ui-kit/core-design/Text'
 import Heading from '../../ui-kit/core-design/Heading'
 import SubHeading from '../../ui-kit/core-design/SubHeading'
 import PictureForAllResolutions from '../../ui-kit/PictureForAllResolutions'
 import { MsBrowserConsumer } from '../../../utils/msBrowserProvider'
+import FactItem from '../FactItem'
 
-const FactItem = ({ number, text, itemClassName }) => (
-  <div className={cn('fact-item', itemClassName)}>
-    <Heading
-      as="p"
-      type="slab"
-      size="l"
-      dangerouslySetInnerHTML={{ __html: number }}
-      className="fact-number"
-    />
-
-    <Text type="strong" dangerouslySetInnerHTML={{ __html: text }} className="fact-text" />
-  </div>
-)
-
-const Facts = ({ className, id, content: { heading, subHeading, images, imgAlt, factItems } }) => (
+const Facts = ({
+  className,
+  id,
+  t,
+  content: { heading, subHeading, images, imgAlt, factItems },
+}) => (
   <Grid className={className} as="section" id={id}>
     <Heading.H2
       type="slab"
-      size="l"
-      dangerouslySetInnerHTML={{ __html: heading }}
+      size="m"
+      dangerouslySetInnerHTML={{ __html: t(heading) }}
       className="heading"
     />
 
     <SubHeading
       type="slab"
-      dangerouslySetInnerHTML={{ __html: subHeading }}
+      dangerouslySetInnerHTML={{ __html: t(subHeading) }}
       className="sub-heading"
     />
 
     <PictureForAllResolutions
       images={images}
-      fallback={images['desktop.all'].png}
-      alt={imgAlt}
+      fallback={images['desktop.l'].png}
+      alt={t(imgAlt)}
       className="image"
     />
 
     {factItems.map(({ number, text, className }) => (
-      <FactItem number={number} text={text} itemClassName={className} key={className} />
+      <FactItem number={number} text={text} itemClassName={className} key={text} />
     ))}
   </Grid>
 )
@@ -55,10 +46,11 @@ const Facts = ({ className, id, content: { heading, subHeading, images, imgAlt, 
 Facts.propTypes = {
   className: string,
   id: string,
+  t: func,
   content: object,
   factItems: array,
 }
 
-export default styled(MsBrowserConsumer(Facts))`
+export default styled(translate()(MsBrowserConsumer(Facts)))`
   ${styles}
 `

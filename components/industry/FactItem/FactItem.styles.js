735a086a7ec4aa49614150d467e6b53ea5b677b1
Viktor Kosinov <vkosinov@ya.ru>
2020-02-14 15:21:34 +0300

    fix/COM-1243 исправляет отступы после цифр

--- components/industry/FactItem/FactItem.styles.js
+++ components/industry/FactItem/FactItem.styles.js
@@ -1,19 +1,27 @@
 import { css } from '@emotion/core'
+import calcRem from '../../../utils/style/calcRem'
 
-const base = ({ colors }) => css`
+const base = ({ breakpoints: { mobile }, colors }) => css`
   .fact-number {
     font-weight: normal;
   }
-  
+
   .fact-text {
     color: ${colors.secondary.darken100};
+  }
+
+  ${mobile.all} {
+    .fact-text {
+      margin-top: ${calcRem(-4)}
+    }
   }
 `
 
 export default props => {
+  const breakpoints = props.theme.breakpoints
   const colors = props.theme.colors
 
   return css`
-    ${base({ colors })}
+    ${base({breakpoints, colors })}
   `
 }

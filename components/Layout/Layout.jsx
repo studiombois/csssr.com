4e04b7671ceec631a0018704c709fadb58c812ca
Artyom Resh <artyomresh@gmail.com>
2020-02-21 10:59:36 +0300

    fix: добавляет фикс после мержа 89 ветки

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -2,24 +2,19 @@ import React, { Fragment } from 'react'
 import DevTools from '../DevTools'
 import { withRouter } from 'next/router'
 import { string } from 'prop-types'
-import { Global, css } from '@emotion/core'
-import styles from './Layout.styles'
+import { Global } from '@emotion/core'
+import styles, { ie11Styles } from './Layout.styles'
 import Header from '../Header'
 import { MsBrowserConsumer } from '../../utils/msBrowserProvider'
 
 const Layout = ({ children, isIe11 }) => (
   <Fragment>
+    <Header />
     <Global styles={styles} />
     {isIe11 && <Global styles={ie11Styles} />}
 
-  return (
-    <Fragment>
-      <Header />
-      <Global styles={styles.base} />
-      <Global styles={styles.font_faces} />
-      <Global styles={styles.fonts} />
-
     <main id="main">{children}</main>
+    <DevTools />
   </Fragment>
 )
 

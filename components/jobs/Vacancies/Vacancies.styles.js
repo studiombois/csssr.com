a4187313c61972ce2212e9dfe4a57aab97023ddd
Sergei <epcijer@gmail.com>
2020-03-04 17:05:26 +0300

    fix: COM-1353 JOBS | Правит отображение фоновой картинки 2

--- components/jobs/Vacancies/Vacancies.styles.js
+++ components/jobs/Vacancies/Vacancies.styles.js
@@ -5,7 +5,6 @@ const base = css`
   & {
     position: relative;
     padding-top: 13.5rem;
-    z-index: 0;
   }
 
   header,
@@ -57,7 +56,6 @@ const base = css`
     background-position: 50%;
     background-size: auto 100%;
     overflow: hidden;
-    z-index: -1;
   }
 
   .hunter-text {

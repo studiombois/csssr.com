d2176f868eb1de6ae857e0bf2bf359106128a950
Artyom Resh <artyomresh@gmail.com>
2020-03-05 07:25:24 +0300

    fix: COM-1316 добавляет условие для появляение DoubleBottom

--- components/Footer/DoubleBottom/DoubleBottom.styles.js
+++ components/Footer/DoubleBottom/DoubleBottom.styles.js
@@ -11,6 +11,7 @@ const base = ({ breakpoints: { desktop, tablet, mobile }}) => css`
     display: flex;
     width: 100%;
     height: ${calcRem(344)};
+    background-color: white;
   }
 
   ${desktop.all} {

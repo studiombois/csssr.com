482156d4db88cfbf7db843537f0d12282b90ef6f
Artyom Resh <artyomresh@gmail.com>
2020-03-13 17:09:32 +0300

    fix: COM-1329 добавил новое улсловие в getGtmId

--- utils/getGtmId.js
+++ utils/getGtmId.js
@@ -4,7 +4,7 @@ export default language => {
   if (!isProduction) {
     return 'GTM-W23KLZB'
   }
-  if (language === 'en') {
+  if (language === 'en' || language === 'en-sg') {
     return 'GTM-TDG7X5G'
   }
   return 'GTM-K67FHB5'

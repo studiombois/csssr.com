2690d803a05b64cc630a9b6b9385a201602dbedc
Sergei <epcijer@gmail.com>
2020-03-18 11:20:50 +0300

    fix: COM-1457 Форматирование

--- components/ui-kit/core-design/SubHeading.js
+++ components/ui-kit/core-design/SubHeading.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { SubHeading as SubHeadingOrigin } from '@csssr/core-design'
 
 const SubHeading = styled(SubHeadingOrigin)`
-    color: #18191B;
+  color: #18191b;
 `
 
-export default SubHeading;
+export default SubHeading
--- components/ui-kit/core-design/Text.js
+++ components/ui-kit/core-design/Text.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { Text as TextOrigin } from '@csssr/core-design'
 
 const Text = styled(TextOrigin)`
-    color: #18191B;
+  color: #18191b;
 `
 
-export default Text;
+export default Text

4265256ba5b1ce1fda047064a62c05883d2aa343
exbtrg <exbtrg@gmail.com>
2020-03-24 18:50:35 +0300

    release/COM-104: добавляет placeholder для формата pdf

--- utils/getFileFieldText.js
+++ utils/getFileFieldText.js
@@ -1,16 +1,18 @@
 export default t => accept => {
   switch (accept) {
-    case 'zip':
-      return `(${t('job:zipPacked')})`
-    case 'docx':
-      return `(${t('job:docxFormat')})`
-    case 'xlsx':
-      return `(${t('job:xlsxFormat')})`
-    case 'sketch':
-      return `(${t('job:sketchFormat')})`
-    case 'jpg':
-      return `(${t('job:jpgFormat')})`
-    default:
-      return ''
+  case 'zip':
+    return `(${t('job:zipPacked')})`
+  case 'docx':
+    return `(${t('job:docxFormat')})`
+  case 'xlsx':
+    return `(${t('job:xlsxFormat')})`
+  case 'sketch':
+    return `(${t('job:sketchFormat')})`
+  case 'jpg':
+    return `(${t('job:jpgFormat')})`
+  case 'pdf':
+    return `(${t('job:pdfFormat')})`
+  default:
+    return ''
   }
 }

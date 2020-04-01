4a5912d52d6f9ea41f6a188cafcd29ff8ed6be0f
Artyom Resh <artyomresh@gmail.com>
2020-04-01 14:55:51 +0300

    feat: COM-1564 добавялет новый блок Benefits на страницу front-end-outsourcing

---
+++ components/service/outsourcing-front-end/Benefits/List/Item/Item.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { string, func } from 'prop-types'
+import styled from '@emotion/styled'
+import styles from './Item.styles'
+import translate from '../../../../../../utils/translate-wrapper'
+
+import Heading from '../../../../../ui-kit/core-design/Heading'
+import Text from '../../../../../ui-kit/core-design/Text'
+import Picture from '../../../../../ui-kit/Picture'
+
+const Item = ({ className, t, index, images }) => (
+  <li className={className}>
+    <Heading
+      className="title"
+      as="h3"
+      dangerouslySetInnerHTML={{ __html: t(`outsourcingFrontEnd:benefits.list.${index}.title`) }}
+      type="regular"
+      size="m"
+    />
+
+    <Text
+      className="description"
+      as="p"
+      size="m"
+      dangerouslySetInnerHTML={{
+        __html: t(`outsourcingFrontEnd:benefits.list.${index}.description`),
+      }}
+    />
+
+    <Picture
+      className="picture"
+      images={images}
+      fallback={images.png}
+      alt={t('outsourcingFrontEnd:imageAlt.aboutFrontend')}
+    />
+  </li>
+)
+
+Item.propTypes = {
+  className: string,
+  t: func,
+}
+
+export default translate(styled(Item)`
+  ${styles}
+`)
---
+++ components/service/outsourcing-front-end/Benefits/List/List.styles.js
@@ -0,0 +1,90 @@
+import { css } from "@emotion/core";
+import getGridValueForMs from "../../../../../utils/style/getGridValueForMs";
+import calcRem from "../../../../../utils/style/calcRem";
+
+const base = ({ breakpoints: { desktop, tablet, mobile }}) => css`
+  ${desktop.all} {
+    & {
+      margin-top: 155px;
+    }
+  }
+
+  ${desktop.l} {
+    & {
+      grid-column: 8 / span 3;
+    }
+  }
+
+  ${desktop.m} {
+    & {
+      grid-column: 8 / span 4;
+    }
+  }
+
+  ${desktop.s} {
+    & {
+      grid-column: 8 / span 4;
+    }
+  }
+
+  ${tablet.all} {
+    & {
+      margin-top: ${calcRem(55)};
+      grid-column: 8 / span 4;
+    }
+  }
+
+  ${mobile.all} {
+    & {
+      margin-top: ${calcRem(61)};
+      grid-column: 2 / span 5;
+    }
+  }
+`;
+
+const ie11Styles = ({ breakpoints: { desktop, tablet, mobile } }) => css`
+  ${desktop.l} {
+    & {
+      -ms-grid-column: ${getGridValueForMs(8)};
+      -ms-grid-column-span: ${getGridValueForMs(3)};
+    }
+  }
+
+  ${desktop.m} {
+    & {
+      -ms-grid-column: ${getGridValueForMs(8)};
+      -ms-grid-column-span: ${getGridValueForMs(4)};
+    }
+  }
+
+  ${desktop.s} {
+    & {
+      -ms-grid-column: ${getGridValueForMs(8)};
+      -ms-grid-column-span: ${getGridValueForMs(4)};
+    }
+  }
+
+  ${tablet.all} {
+    & {
+      -ms-grid-column: ${getGridValueForMs(8)};
+      -ms-grid-column-span: ${getGridValueForMs(4)};
+    }
+  }
+
+  ${mobile.all} {
+    & {
+      -ms-grid-column: ${getGridValueForMs(2)};
+      -ms-grid-column-span: ${getGridValueForMs(5)};
+    }
+  }
+`;
+
+export default props => {
+  const breakpoints = props.theme.breakpoints;
+  const colors = props.theme.colors;
+
+  return css`
+    ${base({ breakpoints, colors })}
+    ${props.isIe11 && ie11Styles({ breakpoints })}
+  `;
+};

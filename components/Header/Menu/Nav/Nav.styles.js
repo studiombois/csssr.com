db565befae72faeeff2209866a071722d71997e4
Sergei <epcijer@gmail.com>
2020-04-10 15:42:26 +0300

    fix: COM-1631 Правка стилей

--- components/Header/Menu/Nav/Nav.styles.js
+++ components/Header/Menu/Nav/Nav.styles.js
@@ -90,6 +90,42 @@ const base = ({ breakpoints: { tablet, mobile }, colors }) => css`
     .icon {
       top: ${calcRem(4)};
     }
+
+    .nav-item_active .icon_fintech path:last-of-type {
+      fill: #5695ED;
+    }
+
+    .nav-item_active .icon_eCommerce path:last-of-type {
+        fill: #5695ED;
+    }
+
+    .nav-item_active .icon_information path:first-of-type {
+      fill: #5695ED;
+    }
+      
+    .nav-item_active .icon_mediaAndMarketing path:first-of-type {
+      fill: #5695ED;
+    }
+
+    .nav-item_active .icon_mediaAndMarketing path:last-of-type {
+      stroke: #18191B;
+    }
+  }
+
+  .nav_howWeWork {
+    .nav-item_active .icon_technologies {
+      fill: #5695ED;
+    }
+  }
+
+  .nav_services {
+    .nav-item_active .icon path {
+      stroke: #5695ED;
+    }
+
+    .nav-item_active .icon {
+      fill: #5695ED;
+    }
   }
 
   .nav_services_ru,
@@ -148,53 +184,45 @@ const base = ({ breakpoints: { tablet, mobile }, colors }) => css`
       }
     }
 
-    .nav_services {
-      .link:hover {
-        color: #5695ED;
-      }
-
-      .nav-item_active .icon,
-      .link:hover .icon {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon path,
-      .link:hover .icon path {
-        stroke: #5695ED;
-      }
-    }
-
     .nav_industries {
-      .nav-item_active .icon_fintech path:last-of-type,
       .link:hover .icon_fintech path:last-of-type {
         fill: #5695ED;
       }
 
-      .nav-item_active .icon_eCommerce path:last-of-type,
       .link:hover .icon_eCommerce path:last-of-type {
         fill: #5695ED;
       }
 
-      .nav-item_active .icon_information path:first-of-type,
       .link:hover .icon_information path:first-of-type {
         fill: #5695ED;
       }
       
-      .nav-item_active .icon_mediaAndMarketing path:first-of-type,
       .link:hover .icon_mediaAndMarketing path:first-of-type {
         fill: #5695ED;
       }
 
-      .nav-item_active .icon_mediaAndMarketing path:last-of-type,
       .link:hover .icon_mediaAndMarketing path:last-of-type {
         stroke: #18191B;
       }
     }
 
     .nav_howWeWork {
-      .nav-item_active .icon_technologies,
       .link:hover .icon_technologies {
         fill: #5695ED;
+      }
+    }
+
+    .nav_services {
+      .link:hover {
+        color: #5695ED;
+      }
+  
+      .link:hover .icon {
+        fill: #5695ED;
+      }
+
+      .link:hover .icon path {
+        stroke: #5695ED;
       }
     }
   }
@@ -213,40 +241,12 @@ const base = ({ breakpoints: { tablet, mobile }, colors }) => css`
     }
 
     .nav_services {
-      .nav-item_active .icon {
-        fill: #5695ED;
-      }
+      // .nav-item_active .icon {
+      //   fill: #5695ED;
+      // }
 
       .nav-item_active .icon path {
         stroke: #5695ED;
-      }
-    }
-
-    .nav_industries {
-      .nav-item_active .icon_fintech path:last-of-type {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon_eCommerce path:last-of-type {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon_information path:first-of-type {
-        fill: #5695ED;
-      }
-      
-      .nav-item_active .icon_mediaAndMarketing path:first-of-type {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon_mediaAndMarketing path:last-of-type {
-        stroke: #18191B;
-      }
-    }
-
-    .nav_howWeWork {
-      .nav-item_active .icon_technologies {
-        fill: #5695ED;
       }
     }
   }
@@ -349,16 +349,6 @@ const base = ({ breakpoints: { tablet, mobile }, colors }) => css`
       }
     }
 
-    .nav_services {
-      .nav-item_active .icon {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon path {
-        stroke: #5695ED;
-      }
-    }
-
     .nav_industries {
       padding-top: ${calcRem(8)};
 
@@ -388,32 +378,6 @@ const base = ({ breakpoints: { tablet, mobile }, colors }) => css`
       .icon {
         width: ${calcRem(28)};
         height: ${calcRem(28)};
-      }
-
-      .nav-item_active .icon_fintech path:last-of-type {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon_eCommerce path:last-of-type {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon_information path:first-of-type {
-        fill: #5695ED;
-      }
-      
-      .nav-item_active .icon_mediaAndMarketing path:first-of-type {
-        fill: #5695ED;
-      }
-
-      .nav-item_active .icon_mediaAndMarketing path:last-of-type {
-        stroke: #18191B;
-      }
-    }
-
-    .nav_howWeWork {
-      .nav-item_active .icon_technologies {
-        fill: #5695ED;
       }
     }
   }

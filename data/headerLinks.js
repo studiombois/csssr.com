99ba72826125abae6d67cf0be64e1c9c8ccb9d43
Artyom Resh <artyomresh@gmail.com>
2020-04-13 17:30:35 +0300

    fix: COM-1659 добавляет ссылку на английский блог

--- data/headerLinks.js
+++ data/headerLinks.js
@@ -113,10 +113,10 @@ export default {
     },
   ],
   links: [
-    // {
-    //   title: 'common:header.links.blog.title',
-    //   href: 'https://blog.csssr.com',
-    // },
+    {
+      title: 'common:header.links.blog.title',
+      href: 'https://blog.csssr.com',
+    },
     // {
     //   title: 'common:header.links.aboutUs.title',
     //   href: 'about-us',

2b2659f54b29055fbc272d834b022651e5c56151
Artyom Resh <artyomresh@gmail.com>
2020-04-14 15:06:22 +0300

    feat: COM-1673 добавляет в футер ссылки на наши проекты

--- components/Footer/Nav/Nav.styles.js
+++ components/Footer/Nav/Nav.styles.js
@@ -10,6 +10,10 @@ const base = ({ breakpoints: { desktop, tablet, mobile }, colors }) => css`
     justify-content: space-between;
   }
 
+  .nav-item:nth-of-type(4) {
+    width: 100px;
+  }
+
   .title {
     display: block;
     color: ${colors.secondary.darken100};

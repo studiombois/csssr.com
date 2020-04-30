89e33c195fdd08b7a5c7726fd854ea7bff18366c
Sergei <epcijer@gmail.com>
2020-04-30 16:08:54 +0300

    fix: COM-1722 Добавил коммент и удалил formatTime

--- components/Footer/DoubleBottom/Addresses/Addresses.jsx
+++ components/Footer/DoubleBottom/Addresses/Addresses.jsx
@@ -99,7 +99,7 @@ const Addresses = ({ className, isTablet, isMobile, t, isIe11, setHoveredAddress
             )}
             {!isIe11 && 
               <Text className="time" type="regular" size={textSize}>
-                {time.toLocaleTimeString([], { timeZone: timezoneOffsetsByAddressId[id], hour: '2-digit', minute: '2-digit' })}
+                {time.toLocaleTimeString([], { timeZone: timezoneOffsetsByAddressId[id], hour: '2-digit', minute: '2-digit' })} {/* show only hours and minutes, use options with the default locale - use an empty array, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString */}
               </Text>
             }
           </div>

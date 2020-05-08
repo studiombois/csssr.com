72f4ebd54c28bafb6edc3bb47366c0b80a467641
Viktor Kosinov <vkosinov@ya.ru>
2020-05-08 18:53:13 +0300

    COM-1821: Добавил форму обратной связи

--- components/service/backend-devops/BackEndDevopsPage.jsx
+++ components/service/backend-devops/BackEndDevopsPage.jsx
@@ -6,22 +6,22 @@ import translate from '../../../utils/translate-wrapper'
 import Head from '../../Head'
 import Hero from './Hero'
 import DevOpsServices from './DevOpsServices'
+import Form from './Form'
 
 import hero from '../../../data/service/backend-and-devops/hero'
 import devopsServices from '../../../data/service/backend-and-devops/devops-services'
 
 const BackEndDevopsPage = ({ t }) => {
-  const pageName = 'backend-and-devops'
+  const pageName = 'backEndAndDevops'
   return (
     <Layout pageName={pageName}>
       <Head
         title={t('backEndAndDevops:meta.title')}
         description={t('backEndAndDevops:meta.description')}
       />
-
       <Hero id="hero" content={hero} />
-
       <DevOpsServices id="devops-services" content={devopsServices} />
+      <Form pageName={pageName} />
     </Layout>
   )
 }

27cbab23c6df493944dbabc1ef3e88a79793687e
Alexandr Kulakov <BenuelBertran@gmail.com>
2020-06-23 18:21:36 +0300

    Replace login in a function

--- common/get-page-pathname-in-language.js
+++ common/get-page-pathname-in-language.js
@@ -1,13 +1,23 @@
+export const getPathName = (path) => {
+  const [, , , pathname] = path.match(/^\/(.*?)(\/(.*))?$/)
+
+  return pathname
+}
+
+export const isJobPage = (pathname) => {
+  return ['job', 'jobs', 'jobs-faq'].indexOf(pathname) === -1
+}
+
 export default (fullPathname, newLanguage, pagesList) => {
   // Здесь не используются именованные группы для поддержки IE
-  const [, , , pathname] = fullPathname.match(/^\/(.*?)(\/(.*))?$/)
+  const pathname = getPathName(fullPathname)
 
   if (!pathname) {
     // /ru или /en
     return `/${newLanguage}`
   }
 
-  if (['job', 'jobs', 'jobs-faq'].indexOf(pathname) !== -1) {
+  if (isJobPage(pathname)) {
     return `/${newLanguage}/jobs`
   }
 

a37bfa4c08ae27a70218ba6f2a5bbc96613ef026
Alexandr Kulakov <BenuelBertran@gmail.com>
2020-07-17 15:46:40 +0300

    Added new shared header

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,18 +1,27 @@
 import React, { Fragment } from 'react'
 import DevTools from '../DevTools'
 import { withRouter } from 'next/router'
-import Header from '../Header'
+import { Global } from '@emotion/core'
+import Link from 'next/link'
+import styles, { ie11Styles } from './Layout.styles'
+// import Header from '../Header'
+import { Header } from '@csssr/csssr-shared-header'
 import Footer from '../Footer'
 import { MsBrowserConsumer } from '../../utils/msBrowserProvider'
 import CookiesPopup from '../CookiesPopup'
 
-const Layout = ({ children, isIe11, pageName, router: { asPath }, withFooter = true }) => {
+const Layout = ({ children, isIe11, withFooter = true }) => {
   const dynamicTag = isIe11 ? 'div' : 'main'
-  const pathsNoButton = ['jobs']
-  const isButtonVisible = !pathsNoButton.some((string) => asPath.indexOf(string) + 1)
+  // const pathsNoButton = ['jobs']
+  // const isButtonVisible = !pathsNoButton.some((string) => asPath.indexOf(string) + 1)
   return (
     <Fragment>
-      <Header isButtonVisible={isButtonVisible} pageName={pageName} />
+      {/* <Header isButtonVisible={isButtonVisible} pageName={pageName} />
+       */}
+      <Header pathname="dev" isIe11={false} isMobile={true} lng={['ru', 'en']} NextLink={Link} />
+      <Global styles={styles} />
+
+      {isIe11 && <Global styles={ie11Styles} />}
 
       {React.createElement(dynamicTag, { id: 'main' }, children)}
 

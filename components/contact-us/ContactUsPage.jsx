691ed3270b104ba94e1b54fadd0b1cccc092a35f
Artyom Resh <artyomresh@gmail.com>
2020-07-20 17:43:13 +0300

    feat(BookACall): add new component

--- components/contact-us/ContactUsPage.jsx
+++ components/contact-us/ContactUsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../Layout'
 import { L10nConsumer } from '../../utils/l10nProvider'
+import BookACall from './BookACall'
 
 import Head from '../Head'
 import ContactUs from './ContactUs'
@@ -14,8 +15,22 @@ const ContactUsPage = ({ l10n: { translations } }) => {
         title={translations.contactUs.meta.title}
         description={translations.contactUs.meta.description}
       />
-
       <ContactUs />
+
+      <BookACall
+        profileId="anastasia_ignatenko"
+        reservationTime="13:30 - 14:00, Thursday, May 21, 2020"
+        canBookACall
+        wasCallReservationSuccessful
+      />
+      <BookACall
+        profileId="anastasia_vnuchenko"
+        reservationTime="13:30 - 14:00, Thursday, May 21, 2020"
+        canBookACall
+        wasCallReservationSuccessful
+      />
+      <BookACall profileId="olga_shevchenko" />
+      <BookACall profileId="victoria_zubareva" />
     </Layout>
   )
 }
---
+++ data/contact-us/profiles.js
@@ -0,0 +1,30 @@
+import anastasia_ignatenko from '../../static/images/contact-us/book-a-call/anastasia_ignatenko.jpg?responsive'
+import anastasia_ignatenko_webp from '../../static/images/contact-us/book-a-call/anastasia_ignatenko.jpg?responsive_and_webp'
+
+import anastasia_vnuchenko from '../../static/images/contact-us/book-a-call/anastasia_vnuchenko.jpg?responsive'
+import anastasia_vnuchenko_webp from '../../static/images/contact-us/book-a-call/anastasia_vnuchenko.jpg?responsive_and_webp'
+
+import olga_shevchenko from '../../static/images/contact-us/book-a-call/olga_shevchenko.jpg?responsive'
+import olga_shevchenko_webp from '../../static/images/contact-us/book-a-call/olga_shevchenko.jpg?responsive_and_webp'
+
+import victoria_zubareva from '../../static/images/contact-us/book-a-call/victoria_zubareva.jpg?responsive'
+import victoria_zubareva_webp from '../../static/images/contact-us/book-a-call/victoria_zubareva.jpg?responsive_and_webp'
+
+export default {
+  anastasia_ignatenko: {
+    images: { jpg: anastasia_ignatenko, webp: anastasia_ignatenko_webp },
+    email: 'launch@csssr.com',
+  },
+  anastasia_vnuchenko: {
+    images: { jpg: anastasia_vnuchenko, webp: anastasia_vnuchenko_webp },
+    email: 'launch@csssr.com',
+  },
+  olga_shevchenko: {
+    images: { jpg: olga_shevchenko, webp: olga_shevchenko_webp },
+    email: 'launch@csssr.com',
+  },
+  victoria_zubareva: {
+    images: { jpg: victoria_zubareva, webp: victoria_zubareva_webp },
+    email: 'join@csssr.com',
+  },
+}

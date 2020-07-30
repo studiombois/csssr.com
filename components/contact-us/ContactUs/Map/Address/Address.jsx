d6d76217f03d808f3686c5d851dbd2565a73d36b
Viktor Kosinov <vkosinov@ya.ru>
2020-07-30 17:13:35 +0300

    COM-2341: Hide pin no lion

--- components/contact-us/ContactUs/Map/Address/Address.jsx
+++ components/contact-us/ContactUs/Map/Address/Address.jsx
@@ -7,7 +7,7 @@ import { MapConsumer } from '../../../../../utils/mapContext'
 import styled from '@emotion/styled'
 import styles from './Address.styles'
 
-import { ReactComponent as PinIcon } from '../../../../../static/icons/pin.svg'
+// import { ReactComponent as PinIcon } from '../../../../../static/icons/pin.svg'
 import { ReactComponent as PinLionIcon } from '../../../../../static/icons/pin-lion.svg'
 
 import addresses from '../../../../../data/contact-us/address'
@@ -17,7 +17,7 @@ const Address = ({ className, l10n: { translations }, activeAddressId }) => {
 
   return (
     <div className={className}>
-      {id === 'sg' ? <PinLionIcon className="pin" /> : <PinIcon className="pin" />}
+      {id === 'sg' && <PinLionIcon className="pin" />}
 
       <div className="addresses">
         {items.map(({ id, value }) => (

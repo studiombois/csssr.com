a8213481edf591173da86c85ae082ad2d3525269
Artyom Resh <artyomresh@gmail.com>
2020-08-25 10:25:23 +0300

    feat(fonts): COM-2172
    - replace Fonts component

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ import { GtmNoScript, GtmScript } from 'react-gtm-components'
 import getGtmId from '../utils/getGtmId'
 import cn from 'classnames'
 import detectMsBrowser, { detectIe11 } from '../utils/detectMsBrowserByUserAgent'
-import Fonts from '../components/Fonts'
+import { Fonts } from '@csssr/core-design'
 
 // Текст для модального окна невалидного браузера
 const CONTENT = {
@@ -51,7 +51,7 @@ export default class MyDocument extends Document {
       <html lang={language} className={cn({ ie11: isIe11, msBrowser: isMsBrowser })}>
         <Head>
           <GtmScript gtmId={gtmId} />
-          <Fonts />
+          <Fonts preset="com" />
         </Head>
         <body>
           <GtmNoScript gtmId={gtmId} />

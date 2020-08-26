31c00151f9e34c648809c1d19ee39d9a3c1c86b9
slava <slavaavals616@gmail.com>
2020-08-26 13:34:13 +0400

    Added new shared header

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,24 +1,52 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import DevTools from '../DevTools'
 import { withRouter } from 'next/router'
-import Header from '../Header'
+import { Global } from '@emotion/core'
+import Link from 'next/link'
+import styles from './Layout.styles'
+import { Header, getLocaleFromUrl, PageContent, getDevPathname } from '@csssr/csssr-shared-header'
 import Footer from '../Footer'
+import DesktopHeader from '../Header'
 import { MsBrowserConsumer } from '../../utils/msBrowserProvider'
 import CookiesPopup from '../CookiesPopup'
+import { DeviceContext } from '../../utils/deviceProvider'
 
-const Layout = ({ children, isIe11, pageName, router: { asPath }, withFooter = true }) => {
+import spaceOrigin from '../../utils/csssrSpaceOrigin'
+
+const Layout = ({ children, isIe11, withFooter = true, router, pageName }) => {
+  const { isMobile } = useContext(DeviceContext)
+
   const dynamicTag = isIe11 ? 'div' : 'main'
+  const appRootElement = typeof window === 'object' ? document.getElementById('__next') : null
+  const lng = getLocaleFromUrl(router.asPath)
+
   const pathsNoButton = ['jobs']
-  const isButtonVisible = !pathsNoButton.some((string) => asPath.indexOf(string) + 1)
+  const isButtonVisible = !pathsNoButton.some((string) => router.asPath.indexOf(string) + 1)
+
+  const pathname = getDevPathname(router.asPath)
+
   return (
     <Fragment>
-      <Header isButtonVisible={isButtonVisible} pageName={pageName} />
+      {isMobile ? (
+        <Header
+          isMobile={isMobile}
+          appRootElement={appRootElement}
+          pathname={pathname}
+          lng={lng}
+          NextLink={Link}
+          jobsDomain={spaceOrigin}
+        />
+      ) : (
+        <DesktopHeader isButtonVisible={isButtonVisible} pageName={pageName} />
+      )}
 
-      {React.createElement(dynamicTag, { id: 'main', 'data-testid': 'Main:block' }, children)}
-
-      {withFooter && <Footer />}
-      <CookiesPopup />
-      <DevTools />
+      <PageContent>
+        <Global styles={styles} />
+        {React.createElement(dynamicTag, { id: 'main', 'data-testid': 'Main:block' }, children)}
+        {withFooter && <Footer />}
+        <CookiesPopup />
+        <DevTools />
+      </PageContent>
     </Fragment>
   )
 }

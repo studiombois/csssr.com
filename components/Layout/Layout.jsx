3f2a1f3f63bb2d28cf408058fe7c7aed3c0b72d5
slava <slavaavals616@gmail.com>
2020-09-17 11:58:51 +0400

    feat(desktop-header): COM-2711

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -6,39 +6,31 @@ import Link from 'next/link'
 import styles from './Layout.styles'
 import { Header, getLocaleFromUrl, PageContent, getDevPathname } from '@csssr/csssr-shared-header'
 import Footer from '../Footer'
-import DesktopHeader from '../Header'
 import { MsBrowserConsumer } from '../../utils/msBrowserProvider'
 import CookiesPopup from '../CookiesPopup'
 import { DeviceContext } from '../../utils/deviceProvider'
 
 import spaceOrigin from '../../utils/csssrSpaceOrigin'
 
-const Layout = ({ children, isIe11, withFooter = true, router, pageName }) => {
+const Layout = ({ children, isIe11, withFooter = true, router }) => {
   const { isMobile } = useContext(DeviceContext)
 
   const dynamicTag = isIe11 ? 'div' : 'main'
   const appRootElement = typeof window === 'object' ? document.getElementById('__next') : null
   const lng = getLocaleFromUrl(router.asPath)
 
-  const pathsNoButton = ['jobs']
-  const isButtonVisible = !pathsNoButton.some((string) => router.asPath.indexOf(string) + 1)
-
   const pathname = getDevPathname(router.asPath)
 
   return (
     <Fragment>
-      {isMobile ? (
-        <Header
-          isMobile={isMobile}
-          appRootElement={appRootElement}
-          pathname={pathname}
-          lng={lng}
-          NextLink={Link}
-          jobsDomain={spaceOrigin}
-        />
-      ) : (
-        <DesktopHeader isButtonVisible={isButtonVisible} pageName={pageName} />
-      )}
+      <Header
+        isMobile={isMobile}
+        appRootElement={appRootElement}
+        pathname={pathname}
+        lng={lng}
+        NextLink={Link}
+        jobsDomain={spaceOrigin}
+      />
 
       <PageContent>
         <Global styles={styles} />

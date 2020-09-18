5634694d03794ea4156016992e77e6d778e650ea
Sergei <epcijer@gmail.com>
2020-09-18 13:08:31 +0300

    fix: Job page| spread initial values

--- components/job/JobPage.jsx
+++ components/job/JobPage.jsx
@@ -98,8 +98,6 @@ const filterUncheckedContactOptions = (values, translations) => {
 const onSubmit = (translations) => async (values) => {
   const filteredValues = filterUncheckedContactOptions(values, translations)
 
-  filteredValues.privacyPolicy = true
-
   if (values.quests.length === 0) {
     delete filteredValues.quests
   }
@@ -210,7 +208,7 @@ class JobPage extends PureComponent {
             vacancy={vacancy}
             language={language}
             vacancies={vacancies}
-            initialValues={initialValues}
+            initialValues={{ ...initialValues, privacyPolicy: true }}
             validate={candidateFormValidationRules(vacancy, translations, language)}
             onSubmit={onSubmit(translations)}
             decorators={[focusOnErrors]}

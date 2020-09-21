d49d22714e4a6d35dded5014f80d46353771f383
mikhalenko <anna.mikhalenko@csssr.dev>
2020-09-22 01:41:50 +0300

    COM-2673-e2e

--- e2e-tests/nightwatch/tests/01 Main/02 Sending request.test.js
+++ e2e-tests/nightwatch/tests/01 Main/02 Sending request.test.js
@@ -3,11 +3,11 @@ const { modalContInfo } = require('../actions/modalContInfo')
 
 // Отправка заявок с русской локали
 
-testcase('Send request from header modal form of RU-locales | Technologies', () => {
-  step('Go to Technology page', () => {
+testcase('Send request from header modal form of RU-locales | Main', () => {
+  step('Go to Main page', () => {
     browser
       .setWindowSize(1360, 1024)
-      .url(browser.launch_url + '/ru/tech-stack')
+      .url(browser.launch_url + '/ru')
   })
 
   step('Press the button in the header [Нанять нас]', () => {
@@ -17,9 +17,9 @@ testcase('Send request from header modal form of RU-locales | Technologies', () 
   modalContInfo('ru')
 })
 
-testcase('Sending request from the form at the end of the page of RU-locales | Technologies', () => {
+testcase('Sending request from the form at the end of the page of RU-locales | Main', () => {
   step('Go to the form at the end of the page', () => {
-    browser.url(browser.launch_url + '/ru/tech-stack')
+    browser.url(browser.launch_url + '/ru')
   })
 
   contInfo('ru')
@@ -27,11 +27,11 @@ testcase('Sending request from the form at the end of the page of RU-locales | T
 
 // Отправка заявок с английской локали
 
-testcase('Send request from header modal form of EN-locales | Technologies', () => {
-  step('Go to Technology page', () => {
+testcase('Send request from header modal form of EN-locales | Main', () => {
+  step('Go to Main page', () => {
     browser
       .setWindowSize(1360, 1024)
-      .url(browser.launch_url + '/en/tech-stack')
+      .url(browser.launch_url + '/en')
   })
 
   step('Press the button in the header [Hire us]', () => {
@@ -41,9 +41,9 @@ testcase('Send request from header modal form of EN-locales | Technologies', () 
   modalContInfo('en')
 })
 
-testcase('Sending request from the form at the end of the page of EN-locales | Technologies', () => {
+testcase('Sending request from the form at the end of the page of EN-locales | Main', () => {
   step('Go to the form at the end of the page', () => {
-    browser.url(browser.launch_url + '/en/tech-stack')
+    browser.url(browser.launch_url + '/en')
   })
 
   contInfo('en')
---
+++ e2e-tests/nightwatch/tests/08 E-Commerce/01 Sending request.test.js
@@ -0,0 +1,81 @@
+const { contInfo } = require('../actions/contInfo')
+const { modalContInfo } = require('../actions/modalContInfo')
+
+// Отправка заявок с русской локали
+
+testcase('Send request from header modal form of RU-locales | E-Commerce', () => {
+  step('Go to the Fintech industry page', () => {
+    browser
+      .setWindowSize(1360, 1024)
+      .url(browser.launch_url + '/ru/industry/ecommerce')
+  })
+
+  step('Press the button in the header [Нанять нас]', () => {
+    browser.click('[data-testid="Header:button.contactUs"]')
+  })
+
+  modalContInfo('ru')
+})
+
+testcase(
+  'Send request via the button in the first block of RU-locales | E-Commerce',
+  () => {
+    step('Go to the Fintech industry page', () => {
+      browser
+        .setWindowSize(1360, 1024)
+        .url(browser.launch_url + '/ru/industry/ecommerce')
+    })
+
+    step('Press the button [Нанять нас] in the first block', () => {
+      browser.click('[data-testid="Industry:button.contactUs"]')
+    })
+
+    modalContInfo('ru')
+  })
+
+testcase('Sending request from the form at the end of the page of RU-locales | E-Commerce', () => {
+  step('Go to the form at the end of the page', () => {
+    browser.url(browser.launch_url + '/ru/industry/ecommerce')
+  })
+
+  contInfo('ru')
+})
+
+// Отправка заявок с английской страницы
+
+testcase('Send request from header modal form of EN-locales | E-Commerce', () => {
+  step('Go to the Fintech industry page', () => {
+    browser
+      .setWindowSize(1360, 1024)
+      .url(browser.launch_url + '/en/industry/ecommerce')
+  })
+
+  step('Press the button in the header [Hire us]', () => {
+    browser.click('[data-testid="Header:button.contactUs"]')
+  })
+
+  modalContInfo('en')
+})
+
+testcase(
+  'Send request via the button in the first block of EN-locales | E-Commerce',
+  () => {
+    step('Go to the Fintech industry page', () => {
+      browser.setWindowSize(1360, 1024)
+      browser.url(browser.launch_url + '/en/industry/ecommerce')
+    })
+
+    step('Press the button [Hire us] in the first block', () => {
+      browser.click('[data-testid="Industry:button.contactUs"]')
+    })
+
+    modalContInfo('en')
+  })
+
+testcase('Sending request from the form at the end of the page of EN-locales | E-Commerce', () => {
+  step('Go to the form at the end of the page', () => {
+    browser.url(browser.launch_url + '/en/industry/ecommerce')
+  })
+
+  contInfo('en')
+})

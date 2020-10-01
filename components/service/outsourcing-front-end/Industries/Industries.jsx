1a6842aee26fc7533cd9011e2e159c4f4d0c59dd
Artyom Resh <artyomresh@gmail.com>
2020-10-01 17:21:28 +0300

    feat(main): COM-2736 remove and replace components
    - remove Form from main page
    - change Industries origin from main folder to service since it has been removed from main page and now exists only on outsourcing-front-end page
    - replace and refactor Card components, because now two pages share it
    - fix header styles

--- components/service/outsourcing-front-end/Industries/Industries.jsx
+++ components/service/outsourcing-front-end/Industries/Industries.jsx
@@ -1,15 +1,62 @@
 import React from 'react'
 import { string } from 'prop-types'
 import styled from '@emotion/styled'
+import cn from 'classnames'
 import styles from './Industries.styles'
-import IndustriesOrigin from '../../../main/Industries'
 
-const Industries = ({ className }) => <IndustriesOrigin className={className} />
+import Card from '../../../Card'
+import Heading from '../../../ui-kit/core-design/Heading'
+import SubHeading from '../../../ui-kit/core-design/SubHeading'
+import Grid from '../../../ui-kit/core-design/Grid'
+
+import industries from '../../../../data/service/outsourcing-front-end/industries'
+
+import { L10nConsumer } from '../../../../utils/l10nProvider'
+import { MsBrowserConsumer } from '../../../../utils/msBrowserProvider'
+
+const Industries = ({ className, l10n: { translations, language } }) => {
+  return (
+    <Grid as="article" className={cn('industries', className)} data-testid="Home:block.industries">
+      <Heading
+        className="title"
+        as="h2"
+        dangerouslySetInnerHTML={{ __html: translations.outsourcingFrontEnd.industries.title }}
+        type="slab"
+        size="m"
+      />
+
+      <SubHeading
+        className="subtitle"
+        as="p"
+        type="slab"
+        dangerouslySetInnerHTML={{ __html: translations.outsourcingFrontEnd.industries.subtitle }}
+      />
+
+      {industries.map(({ id, testId, title, description, alt, href, images, imagesHovered }) => (
+        <Card
+          className="card"
+          key={id}
+          id={id}
+          testId={testId}
+          title={title}
+          description={description}
+          href={`/${language}/industry/${href}`}
+          images={images}
+          imagesHovered={imagesHovered}
+          alt={alt}
+          isNextLink
+        />
+      ))}
+    </Grid>
+  )
+}
 
 Industries.propTypes = {
   className: string,
 }
 
-export default styled(Industries)`
-  ${styles}
-`
+export default L10nConsumer(
+  MsBrowserConsumer(styled(Industries)`
+    ${styles}
+  `),
+)

d9da209094113bd28d208353167f196f444527a9
Artyom Resh <artyomresh@gmail.com>
2020-10-09 18:13:24 +0300

    fix(Hero): COM-2795 change text

--- components/main/Hero/Hero.styles.js
+++ components/main/Hero/Hero.styles.js
@@ -9,7 +9,7 @@ const base = ({ breakpoints: { desktop, tablet, mobile }, colors}) => css`
     font-weight: normal;
     color: ${colors.secondary.darken100};
 
-    & > span {
+    & > b {
       display: block;
       font-family: Roboto, sans-serif;
       font-weight: 900;

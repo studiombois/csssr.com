032782b1013ca9eff5ea0ea8986671a2576f81c5
Artyom Resh <artyomresh@gmail.com>
2020-10-22 15:57:05 +0300

    feat(Hero): COM-2888 end of Hero ab test
    - remove bad variants data
    - revert ru Hero

--- components/main/Hero/Hero.jsx
+++ components/main/Hero/Hero.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { string } from 'prop-types'
 
-import { Global } from '@emotion/core'
+import { Global, css } from '@emotion/core'
 import styled from '@emotion/styled'
-import styles, { backgroundImagesStyles } from './Hero.styles'
+import { PictureSmart } from '@csssr/csssr.images/dist/react'
+import { backgroundCssSmart } from '@csssr/csssr.images/dist/utils/backgroundCss'
+import styles from './Hero.styles'
 
-import AbContext from '../../../utils/abContext'
 import { L10nConsumer } from '../../../utils/l10nProvider'
 import { DeviceConsumer } from '../../../utils/deviceProvider'
 import { MsBrowserConsumer } from '../../../utils/msBrowserProvider'
 
 import Heading from '../../ui-kit/core-design/Heading'
+import SubHeading from '../../ui-kit/core-design/SubHeading'
 import Grid from '../../ui-kit/core-design/Grid'
 
-const Hero = ({ className, isMobile, l10n: { translations, language } }) => (
+const heroBgImages = require.context('../../../public/images/main/hero-en?csssr-images')
+const HeroEn = ({ className, translations, isMobile }) => (
   <section className={`${className} hero-wrap`}>
     <Grid>
       <Heading
@@ -23,12 +26,50 @@ const Hero = ({ className, isMobile, l10n: { translations, language } }) => (
         size={isMobile ? 'm' : 'l'}
         dangerouslySetInnerHTML={{ __html: translations.main.hero.title }}
       />
-      <AbContext.Consumer>
-        {(ab) => <Global styles={backgroundImagesStyles(ab, language)} />}
-      </AbContext.Consumer>
+      <Global
+        styles={() => css`
+          ${backgroundCssSmart('.hero-wrap', heroBgImages)}
+        `}
+      />
     </Grid>
   </section>
 )
+
+const heroImages = require.context('../../../public/images/main/hero-ru?csssr-images')
+const HeroRu = ({ className, translations, isMobile }) => (
+  <Grid as="article" className={className}>
+    <Heading
+      className="title"
+      as="h1"
+      dangerouslySetInnerHTML={{ __html: translations.main.hero.title }}
+      type="slab"
+      size={isMobile ? 'm' : 'l'}
+    />
+
+    <div className="picture-wrapper">
+      <div className="picture-hover-area" />
+      <PictureSmart
+        className="picture"
+        requireImages={heroImages}
+        testId="Home:img.big-logo"
+        alt={translations.main.imgAlt.hero}
+      />
+    </div>
+    <SubHeading
+      className="description"
+      as="p"
+      type="slab"
+      dangerouslySetInnerHTML={{ __html: translations.main.hero.description }}
+    />
+  </Grid>
+)
+
+const Hero = ({ className, isMobile, l10n: { translations, language } }) =>
+  language === 'ru' ? (
+    <HeroRu className={className} translations={translations} isMobile={isMobile} />
+  ) : (
+    <HeroEn className={className} translations={translations} isMobile={isMobile} />
+  )
 
 Hero.propTypes = {
   className: string,

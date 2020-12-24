7ee71823143ade2267ffb856dd31dec29df9d152
Alexandr Kulakov <BenuelBertran@gmail.com>
2020-12-24 16:25:26 +0300

    [COM-3024]: Added Challenge section

---
+++ components/project/Challenge/Challenge.jsx
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from 'react'
+import { object, string } from 'prop-types'
+import { PictureSmart } from '@csssr/csssr.images/dist/react'
+import styled from '@emotion/styled'
+import styles from './Challenge.styles'
+
+import Grid from '../../ui-kit/core-design/Grid'
+import Heading from '../../ui-kit/core-design/Heading'
+import Text from '../../ui-kit/core-design/Text'
+
+import { L10nConsumer } from '../../../utils/l10nProvider'
+import { DeviceConsumer } from '../../../utils/deviceProvider'
+import { MsBrowserConsumer } from '../../../utils/msBrowserProvider'
+const Challenge = ({
+  className,
+  l10n: { translations, language },
+  isMobile,
+  content: { images, text },
+}) => {
+  const [offset, setOffset] = useState(0)
+  const isWindowContext = typeof window !== 'undefined'
+  let offsetMin = 0
+  let offsetMax = 0
+
+  if (isWindowContext && window.innerWidth >= 1920) {
+    offsetMin = 1000
+    offsetMax = 2100
+  } else if (isWindowContext && window.innerWidth >= 1360 && window.innerWidth < 1920) {
+    offsetMin = 1150
+    offsetMax = 2000
+  } else if (isWindowContext && window.innerWidth >= 1260 && window.innerWidth < 1360) {
+    offsetMin = 1250
+    offsetMax = 2050
+  } else if (isWindowContext && window.innerWidth >= 768 && window.innerWidth < 1260) {
+    offsetMin = 700
+    offsetMax = 1290
+  }
+
+  useEffect(() => {
+    window.onscroll = () => {
+      const offset = window.pageYOffset
+
+      if (offset >= offsetMin && offset <= offsetMax) {
+        setOffset(offset)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <Grid className={className} as="section">
+      <div className="profile">
+        <div className="sticky-box">
+          <div className="wrapper">
+            <div className="picture-container">
+              <div
+                style={{
+                  transform: isMobile ? 'translateY(0)' : `translateY(-${offset - offsetMin}px)`,
+                }}
+              >
+                <PictureSmart
+                  requireImages={language === 'ru' ? images.profileRu : images.profileEn}
+                  alt={images.profileAlt(translations)}
+                />
+              </div>
+            </div>
+            <PictureSmart
+              requireImages={images.laptop}
+              alt={images.laptopAlt(translations)}
+              className="laptop-image"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="info">
+        <Heading.H2
+          type="slab"
+          size="m"
+          dangerouslySetInnerHTML={{ __html: text.challenge.heading(translations) }}
+          className="heading"
+        />
+        <Text
+          type="strong"
+          dangerouslySetInnerHTML={{ __html: text.challenge.text(translations) }}
+          className="info-text"
+        />
+
+        <Heading.H2
+          type="slab"
+          size="m"
+          dangerouslySetInnerHTML={{ __html: text.task.heading(translations) }}
+          className="heading"
+        />
+        <Text
+          type="strong"
+          dangerouslySetInnerHTML={{ __html: text.task.text(translations) }}
+          className="info-text"
+        />
+
+        <Heading.H2
+          type="slab"
+          size="m"
+          dangerouslySetInnerHTML={{ __html: text.result.heading(translations) }}
+          className="heading"
+        />
+        <div className="result-container">
+          <div className="result-wrapper">
+            <Text
+              as="span"
+              dangerouslySetInnerHTML={{ __html: text.result.months.number(translations) }}
+              className="result-number"
+            />
+            <Text
+              as="span"
+              dangerouslySetInnerHTML={{ __html: text.result.months.description(translations) }}
+              className="result-description"
+            />
+          </div>
+          <div>
+            <Text
+              as="span"
+              dangerouslySetInnerHTML={{ __html: text.result.releases.number(translations) }}
+              className="result-number"
+            />
+            <Text
+              as="span"
+              dangerouslySetInnerHTML={{ __html: text.result.releases.description(translations) }}
+              className="result-description"
+            />
+          </div>
+        </div>
+
+        <div className="quote-wrapper">
+          <Text
+            type="strong"
+            dangerouslySetInnerHTML={{ __html: text.quote.text(translations) }}
+            className="quote-text"
+          />
+          <Text
+            type="strong"
+            as="span"
+            dangerouslySetInnerHTML={{ __html: text.quote.author(translations) }}
+            className="quote-author"
+          />
+          <Text
+            as="span"
+            dangerouslySetInnerHTML={{ __html: text.quote.role(translations) }}
+            className="quote-role"
+          />
+        </div>
+      </div>
+    </Grid>
+  )
+}
+
+Challenge.propTypes = {
+  className: string,
+  content: object,
+}
+
+export default L10nConsumer(
+  DeviceConsumer(
+    MsBrowserConsumer(styled(Challenge)`
+      ${styles}
+    `),
+  ),
+)
--- components/project/GazpromNeftPage.jsx
+++ components/project/GazpromNeftPage.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import Idea from '../Idea'
-import Quote from './Quote'
-import AboutProject from './AboutProject'
-import Layout from '../Layout'
-import { L10nConsumer } from '../../utils/l10nProvider'
 
+import Layout from '../Layout'
 import Head from '../Head'
 import Hero from './Hero'
-import Team from './Team'
+import Challenge from './Challenge'
 import TechStack from './TechStack'
+import AboutProject from './AboutProject'
+import Quote from './Quote'
+import Idea from '../Idea'
+
+import { L10nConsumer } from '../../utils/l10nProvider'
 
 import hero from '../../data/project/gazprom-neft/hero'
-import team from '../../data/project/gazprom-neft/team'
+import challenge from '../../data/project/gazprom-neft/challenge'
 import techStack from '../../data/project/gazprom-neft/techStack'
-import quoteImages from '../../data/project/gazprom-neft/quote'
 import aboutProjectImages from '../../data/project/aboutProject'
+import quoteImages from '../../data/project/gazprom-neft/quote'
 
 const GazpromNeftPage = ({ l10n: { translations } }) => {
   const pageName = 'project'
@@ -32,7 +33,7 @@ const GazpromNeftPage = ({ l10n: { translations } }) => {
       />
 
       <Hero id="hero" content={hero} />
-      <Team id="team" content={team} />
+      <Challenge content={challenge} />
       <TechStack id="teckStack" content={techStack} />
       <AboutProject
         projectId={projectId}

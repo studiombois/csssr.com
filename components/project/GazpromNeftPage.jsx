56ce1d95c1a4464559af9d1d5e4fdd28a24f7c10
Sergei <epcijer@gmail.com>
2020-12-30 12:34:13 +0300

    feat: COM-3027 Remove p4 page blocks

--- components/project/GazpromNeftPage.jsx
+++ components/project/GazpromNeftPage.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
-import Idea from '../Idea'
-import Quote from './Quote'
-import AboutProject from './AboutProject'
 import Layout from '../Layout'
 import { L10nConsumer } from '../../utils/l10nProvider'
 
 import Head from '../Head'
-import Hero from './Hero'
-import Team from './Team'
 import BusinessAnalysis from './BusinessAnalysis'
 
-import hero from '../../data/project/gazprom-neft/hero'
-import team from '../../data/project/gazprom-neft/team'
-import quoteImages from '../../data/project/gazprom-neft/quote'
-import aboutProjectImages from '../../data/project/aboutProject'
 import businessAnalysis from '../../data/project/gazprom-neft/businessAnalysis'
 
 const GazpromNeftPage = ({ l10n: { translations } }) => {
   const pageName = 'project'
-  const projectId = 'gazpromNeft'
-  const aboutProjectParagraphsScheme = [
-    [0, 1, 2],
-    [0, 1, 2],
-  ]
 
   return (
     <Layout pageName={pageName}>
@@ -31,16 +17,7 @@ const GazpromNeftPage = ({ l10n: { translations } }) => {
         description={translations.project.gazpromNeft.meta.description}
       />
 
-      <Hero id="hero" content={hero} />
-      <Team id="team" content={team} />
-      <AboutProject
-        projectId={projectId}
-        images={aboutProjectImages}
-        paragraphsScheme={aboutProjectParagraphsScheme}
-      />
       <BusinessAnalysis content={businessAnalysis} />
-      <Quote projectId={projectId} images={quoteImages} />
-      <Idea pageName={pageName} />
     </Layout>
   )
 }

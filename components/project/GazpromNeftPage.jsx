7cc61b13a0d467a3531b7cf364c29f0be2159680
Sergei <epcijer@gmail.com>
2020-12-30 12:39:14 +0300

    feat: COM-3028 Remove p4 page blocks

--- components/project/GazpromNeftPage.jsx
+++ components/project/GazpromNeftPage.jsx
@@ -1,24 +1,14 @@
 import React from 'react'
-import Idea from '../Idea'
-import Quote from './Quote'
 import Development from './Development'
 import Layout from '../Layout'
 import { L10nConsumer } from '../../utils/l10nProvider'
 
 import Head from '../Head'
-import Hero from './Hero'
-import Team from './Team'
-import TechStack from './TechStack'
 
-import hero from '../../data/project/gazprom-neft/hero'
-import team from '../../data/project/gazprom-neft/team'
-import techStack from '../../data/project/gazprom-neft/techStack'
-import quoteImages from '../../data/project/gazprom-neft/quote'
 import development from '../../data/project/gazprom-neft/development'
 
 const GazpromNeftPage = ({ l10n: { translations } }) => {
   const pageName = 'project'
-  const projectId = 'gazpromNeft'
 
   return (
     <Layout pageName={pageName}>
@@ -27,12 +17,7 @@ const GazpromNeftPage = ({ l10n: { translations } }) => {
         description={translations.project.gazpromNeft.meta.description}
       />
 
-      <Hero id="hero" content={hero} />
-      <Team id="team" content={team} />
-      <TechStack id="teckStack" content={techStack} />
       <Development content={development} />
-      <Quote projectId={projectId} images={quoteImages} />
-      <Idea pageName={pageName} />
     </Layout>
   )
 }

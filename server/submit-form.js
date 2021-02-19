75b30c9713cce84fabe7630412d77cf56682c6aa
ArtyomResh <artyom.resh@csssr.com>
2021-02-19 09:03:55 +0300

    feat(amo): COM-3079 add new field to amo payload

--- server/submit-form.js
+++ server/submit-form.js
@@ -2,6 +2,7 @@ import testEmails from '../utils/testEmails'
 
 const Sentry = require('@sentry/node')
 const { sales, TEST_TAG } = require('@csssr/csssr-amo')
+const { google } = require('googleapis')
 const { isProduction } = require('../utils/app-environment')
 const validateFormFields = require('./validate-form-fields')
 const amoSales = sales.init(
@@ -21,24 +22,61 @@ module.exports = async (req, res) => {
     return res.status(400).send({ error: validationResult.errors })
   }
 
-  const tagsArray = ['csssr.com'].concat(pageName)
+  const auth = new google.auth.GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/analytics.readonly'],
+  })
 
-  if (!isProduction) {
-    tagsArray.push(TEST_TAG)
-  }
+  const analyticsreporting = google.analyticsreporting({
+    version: 'v4',
+    auth,
+  })
 
-  if (newsletter) {
-    tagsArray.push('Подписчик')
-  }
-
-  tagsArray.push(language)
-
-  const tags = tagsArray.join(',')
-
-  const { utm_source, utm_medium, utm_campaign, utm_term, utm_content } = req.cookies
-
-  const isTestEmail = testEmails.includes(email)
   try {
+    const getViewId = (language) => {
+      if (!isProduction) {
+        return '185068915'
+      }
+      if (language === 'en') {
+        return '180550356'
+      }
+      return '179131341'
+    }
+
+    const analyticsreportingResponse = await analyticsreporting.userActivity.search({
+      requestBody: {
+        dateRange: {
+          startDate: '60DaysAgo',
+          endDate: 'today',
+        },
+        user: {
+          type: 'CLIENT_ID',
+          userId: gacid,
+        },
+        viewId: getViewId(language),
+      },
+    })
+
+    const { sessions } = analyticsreportingResponse.data
+    const oldestSession = sessions[sessions && sessions.length - 1]
+    const { activities } = oldestSession
+    const oldestActivity = activities[activities && activities.length - 1]
+    const { channelGrouping } = oldestActivity
+    const tagsArray = ['csssr.com'].concat(pageName)
+
+    if (!isProduction) {
+      tagsArray.push(TEST_TAG)
+    }
+
+    if (newsletter) {
+      tagsArray.push('Подписчик')
+    }
+
+    tagsArray.push(language)
+
+    const tags = tagsArray.join(',')
+    const { utm_source, utm_medium, utm_campaign, utm_term, utm_content } = req.cookies
+    const isTestEmail = testEmails.includes(email)
+
     const amoResponse = await amoSales.createLead(isTestEmail ? TEST_TAG : language, {
       tags,
       name,
@@ -47,6 +85,7 @@ module.exports = async (req, res) => {
       message,
       newsletter,
       gacid,
+      channelGrouping,
       utm_source,
       utm_medium,
       utm_campaign,
